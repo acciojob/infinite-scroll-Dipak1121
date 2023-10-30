2a7259ec1204f017b83cfd5338ed45c21550676d
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 15:44:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
-
+let list = document.getElementById("infi-list");
+list.addEventListener("scroll", onScroll);
+function onScroll(){
+	if((list.scrollHeight - list.clientHeight - list.scrollTop) <= 10){
+		addItems();
+		addItems();
+	}
+}
+function addItems(){
+	for ( let i = 11; i <= 19; i++ ){
+		let item = document.createElement("li");
+		item.innerText = `${i}`;
+		list.appendChild(item);
+	}
+}
